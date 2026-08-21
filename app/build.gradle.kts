0d3c7aab2f60ddbea94cad30a2aefc94119ee666
iojh2021-oss <iojh2021@gmail.com>
2026-08-21 13:11:11 +0330

    Upgrade viewer UI and Android dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,25 +11,29 @@ android {
         applicationId = "com.example.screenstreamviewer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     buildTypes {
-        release { isMinifyEnabled = false }
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions { jvmTarget = "1.8" }
+    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("io.getstream:stream-webrtc-android:1.1.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
